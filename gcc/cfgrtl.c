f5dcba9f3986a38355c0863201f054f9f6e89236
dmalcolm <dmalcolm@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-08-19 21:22:25 +0000

    Return types of unlink_insn_chain and duplicate_insn_chain
    2014-08-19  David Malcolm  <dmalcolm@redhat.com>
    
    	* rtl.h (unlink_insn_chain): Strengthen return type from rtx to
    	rtx_insn *.
    	(duplicate_insn_chain): Likewise.
    	* cfgrtl.c (unlink_insn_chain): Strengthen return type from rtx to
    	rtx_insn *, also for locals "prevfirst" and "nextlast".  Add a
    	checked cast for now (until we can strengthen the params in the
    	same way).
    	(duplicate_insn_chain): Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@214197 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cfgrtl.c
+++ gcc/cfgrtl.c
@@ -3343,11 +3343,11 @@ fixup_abnormal_edges (void)
 
 /* Cut the insns from FIRST to LAST out of the insns stream.  */
 
-rtx
+rtx_insn *
 unlink_insn_chain (rtx first, rtx last)
 {
-  rtx prevfirst = PREV_INSN (first);
-  rtx nextlast = NEXT_INSN (last);
+  rtx_insn *prevfirst = PREV_INSN (first);
+  rtx_insn *nextlast = NEXT_INSN (last);
 
   SET_PREV_INSN (first) = NULL;
   SET_NEXT_INSN (last) = NULL;
@@ -3359,7 +3359,7 @@ unlink_insn_chain (rtx first, rtx last)
     set_last_insn (prevfirst);
   if (!prevfirst)
     set_first_insn (nextlast);
-  return first;
+  return as_a <rtx_insn *> (first);
 }
 
 /* Skip over inter-block insns occurring after BB which are typically
@@ -4127,7 +4127,7 @@ cfg_layout_can_duplicate_bb_p (const_basic_block bb)
   return true;
 }
 
-rtx
+rtx_insn *
 duplicate_insn_chain (rtx from, rtx to)
 {
   rtx insn, next, copy;
@@ -4213,7 +4213,7 @@ duplicate_insn_chain (rtx from, rtx to)
     }
   insn = NEXT_INSN (last);
   delete_insn (last);
-  return insn;
+  return as_a <rtx_insn *> (insn);
 }
 
 /* Create a duplicate of the basic block BB.  */
