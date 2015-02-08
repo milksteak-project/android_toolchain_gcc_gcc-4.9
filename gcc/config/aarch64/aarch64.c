adcc2f3cd44a722a6a942ee01008fbe19a366e2d
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-02-08 21:53:21 +0000

    2015-02-08  Andrew Pinski  <apinski@cavium.com>
            * config/aarch64/aarch64.c (gty_dummy): Delete.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220522 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -460,9 +460,6 @@ static const struct aarch64_option_extension all_extensions[] =
    increment address.  */
 static enum machine_mode aarch64_memory_reference_mode;
 
-/* Used to force GTY into this file.  */
-static GTY(()) int gty_dummy;
-
 /* A table of valid AArch64 "bitmask immediate" values for
    logical instructions.  */
 
