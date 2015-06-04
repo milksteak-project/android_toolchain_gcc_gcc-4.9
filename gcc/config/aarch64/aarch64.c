879112d88910b2a2c1f625369ce676919644c966
ktkachov <ktkachov@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-04 08:29:31 +0000

    [AArch64] Always register fma_steering pass but gate it on the target option instead
    	* config/aarch64/aarch64.c (aarch64_override_options): Unconditionally
    	register fma steering pass.
    	* config/aarch64/cortex-a57-fma-steering.c (gate): Add gating on
    	AARCH64_TUNE_FMA_STEERING.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@224116 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -6640,7 +6640,7 @@ aarch64_override_options (void)
     }
 
   if (AARCH64_TUNE_FMA_STEERING)
-    aarch64_register_fma_steering ();
+  aarch64_register_fma_steering ();
 
   aarch64_override_options_after_change ();
 
